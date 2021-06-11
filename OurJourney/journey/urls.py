70771cd1230fb0d5424cc14cf9c6abdea9627745
Kyle Jacobs <vaultman765@gmail.com>
2021-06-11 12:11:51 -0400

    AddAdventure as class view

--- OurJourney/journey/urls.py
+++ OurJourney/journey/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # /journey/
     path('', views.home, name='home'),
     path('search_adventures/', views.AdventureList.as_view(), name='search_adventures'),
-    path('add_adventure/', views.add_adventure, name='add_adventure'),
+    path('add_adventure/', views.AddAdventure.as_view(), name='add_adventure'),
     path('<int:pk>/', views.AdventureDetail.as_view(), name='detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
