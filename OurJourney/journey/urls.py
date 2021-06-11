a0bc3031fa8bcd8c74ea5c3897e9a08cd37626f9
Kyle Jacobs <vaultman765@gmail.com>
2021-06-11 10:04:35 -0400

    static media update

--- OurJourney/journey/urls.py
+++ OurJourney/journey/urls.py
@@ -1,10 +1,13 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'journey'
 urlpatterns = [
     # /journey/
     path('', views.home, name='home'),
     path('search_adventures/', views.AdventureList.as_view(), name='search_adventures'),
-    path('add_adventure/', views.add_adventure, name='add_adventure')
-]
+    path('add_adventure/', views.add_adventure, name='add_adventure'),
+    path('<int:pk>/', views.AdventureDetail.as_view(), name='detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
