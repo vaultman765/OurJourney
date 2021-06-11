c704b9f83b9c3796568cc440061c75012355615b
Kyle Jacobs <vaultman765@gmail.com>
2021-06-10 22:13:18 -0400

    Updated to match AdventureList class

--- OurJourney/journey/urls.py
+++ OurJourney/journey/urls.py
@@ -5,6 +5,6 @@ app_name = 'journey'
 urlpatterns = [
     # /journey/
     path('', views.home, name='home'),
-    path('search_adventures/', views.search_adventures, name='search_adventures'),
+    path('search_adventures/', views.AdventureList.as_view(), name='search_adventures'),
     path('add_adventure/', views.add_adventure, name='add_adventure')
 ]
