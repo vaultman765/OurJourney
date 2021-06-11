0aa4bca4cfb6353b92a1d9a9d23b62771b7cd96d
Kyle Jacobs <vaultman765@gmail.com>
2021-06-11 10:03:37 -0400

    Updated category and subcategory

--- OurJourney/journey/models.py
+++ OurJourney/journey/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Adventures(models.Model):
-    OUTDOORS = 'OU'
-    INDOORS = 'IN'
-    TRIP = 'TR'
-    OTHER = 'OT'
+    OUTDOORS = 'Outdoors'
+    INDOORS = 'Indoors'
+    TRIP = 'Trip'
+    OTHER = 'Other'
     CATEGORY_CHOICES = [
         (OUTDOORS, 'OUTDOORS'),
         (INDOORS, 'INDOORS'),
@@ -14,11 +14,11 @@ class Adventures(models.Model):
         (OTHER, 'OTHER')
     ]
 
-    EXERCISE = 'EX'
-    RELAX = 'RE'
-    ROMANTIC = 'RO'
-    FUN = 'FU'
-    OTHER = 'OT'
+    EXERCISE = 'Exercise'
+    RELAX = 'Relaxation'
+    ROMANTIC = 'Romantic'
+    FUN = 'Fun'
+    OTHER = 'Other'
     SUB_CATEGORY_CHOICES = [
         (EXERCISE, 'EXERCISE'),
         (RELAX, 'RELAX'),
@@ -28,13 +28,14 @@ class Adventures(models.Model):
     ]
 
     adventure = models.CharField(max_length=500)
-    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=OTHER)
-    subcategory = models.CharField(max_length=2, choices=SUB_CATEGORY_CHOICES, default=OTHER)
-    location = models.CharField(max_length=20)
-    time = models.FloatField(default="")
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=OTHER)
+    subcategory = models.CharField(max_length=10, choices=SUB_CATEGORY_CHOICES, default=OTHER)
+    location = models.CharField(max_length=200)
+    hours = models.FloatField(default='0.0')
     website = models.CharField(max_length=500, default="NA")
     details = models.TextField(max_length=2000, default="")
-    image = models.ImageField(upload_to='images/', default='images/none/no_image.png')
+    image = models.ImageField(upload_to='adventures/planned/images/', default='adventures/planned/none/no_image.png')
 
     def __str__(self):
         return self.adventure
+
