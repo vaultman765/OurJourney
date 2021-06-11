42672a6732e71780d9a7c8aa65e9651db2615e91
Kyle Jacobs <vaultman765@gmail.com>
2021-06-10 22:14:10 -0400

    Created AdventureList class

--- OurJourney/journey/views.py
+++ OurJourney/journey/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.core.paginator import Paginator
+from .models import Adventures
 
 
 # Create your views here.
@@ -9,12 +12,12 @@ def home(request):
     return render(request, 'journey/base.html', context)
 
 
-def search_adventures(request):
-
-    context = {
-
-    }
-    return render(request, 'journey/search_adventures.html', context)
+class AdventureList(ListView):
+    model = Adventures
+    template_name = 'journey/search_adventures.html'
+    context_object_name = 'adventure_list'
+    paginate_by = 12
+    paginate_orphans = 4
 
 
 def add_adventure(request):
