92af5fa55cc1b3b1d0370e5cc5ae18ac79e5c04f
Kyle Jacobs <vaultman765@gmail.com>
2021-06-14 18:59:09 -0400

    Moved media root

--- OurJourney/OurJourney/settings.py
+++ OurJourney/OurJourney/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'journey:home'
 LOGIN_URL = 'login'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'www', 'media')
 MEDIA_URL = '/media/'
 
 
