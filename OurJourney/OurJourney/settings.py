c83bb36cb9a0e68b410cd6e8346ee9a5d97a1346
Kyle Jacobs <vaultman765@gmail.com>
2021-06-14 22:32:46 -0400

    Update

--- OurJourney/OurJourney/settings.py
+++ OurJourney/OurJourney/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'journey:home'
 LOGIN_URL = 'login'
