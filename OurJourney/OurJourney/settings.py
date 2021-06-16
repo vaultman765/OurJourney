03c1097c38303be061ecbce8ae1f2f13bf1ff07b
Kyle Jacobs <vaultman765@gmail.com>
2021-06-16 11:25:26 -0400

    dotenv

--- OurJourney/OurJourney/settings.py
+++ OurJourney/OurJourney/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +82,11 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': os.getenv['RDS_DB_NAME'],
+            'USER': os.getenv['RDS_USERNAME'],
+            'PASSWORD': os.getenv['RDS_PASSWORD'],
+            'HOST': os.getenv['RDS_HOSTNAME'],
+            'PORT': os.getenv['RDS_PORT'],
         }
     }
 else:
