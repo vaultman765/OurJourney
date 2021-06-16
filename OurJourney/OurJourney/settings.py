0223c5080a71ff6545f4509f354477b42528cad7
Kyle Jacobs <vaultman765@gmail.com>
2021-06-16 11:54:25 -0400

    syntax fix

--- OurJourney/OurJourney/settings.py
+++ OurJourney/OurJourney/settings.py
@@ -82,11 +82,11 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv['RDS_DB_NAME'],
-            'USER': os.getenv['RDS_USERNAME'],
-            'PASSWORD': os.getenv['RDS_PASSWORD'],
-            'HOST': os.getenv['RDS_HOSTNAME'],
-            'PORT': os.getenv['RDS_PORT'],
+            'NAME': os.getenv('RDS_DB_NAME'),
+            'USER': os.getenv('RDS_USERNAME'),
+            'PASSWORD': os.getenv('RDS_PASSWORD'),
+            'HOST': os.getenv('RDS_HOSTNAME'),
+            'PORT': os.getenv('RDS_PORT'),
         }
     }
 else:
