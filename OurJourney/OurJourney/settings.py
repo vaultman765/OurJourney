aeaf5f658a5b66cf27414810f8947cdf882b4218
Kyle Jacobs <vaultman765@gmail.com>
2021-06-16 11:22:28 -0400

    Update

--- OurJourney/OurJourney/settings.py
+++ OurJourney/OurJourney/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^kvajoz7he1etw#qt*z06)dfxxnx4-um&a$304)-qrq+m^b5dj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
