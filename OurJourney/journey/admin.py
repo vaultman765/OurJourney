526288511c5dfd3948489d9aac16b4fd6f474212
Kyle Jacobs <vaultman765@gmail.com>
2021-06-26 02:04:11 -0400

    Register TakenAdventures

--- OurJourney/journey/admin.py
+++ OurJourney/journey/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Adventures
+from .models import Adventures, TakenAdventures
 
 # Register your models here.
 admin.site.register(Adventures)
+admin.site.register(TakenAdventures)
