8b413fd8b3a7ade2d9bcc88ee13c39751d871a62
Kyle Jacobs <vaultman765@gmail.com>
2021-06-29 14:02:06 -0400

    Login Form

--- OurJourney/journey/forms.py
+++ OurJourney/journey/forms.py
@@ -7,3 +7,7 @@ class AddAdventureForm(forms.ModelForm):
         model = Adventures
         fields = ['adventure', 'category', 'subcategory', 'location', 'hours', 'website', 'details', 'image']
 
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label=(u'Username'),widget = forms.TextInput(attrs = {'placeholder': 'Username'}))
+    password = forms.CharField(max_length=16, label=(u'Password'))
