d17a1fdd5695c220d249f351d164b1c1c2d73851
Kyle Jacobs <vaultman765@gmail.com>
2021-06-29 14:09:04 -0400

    adventures_taken and login

--- OurJourney/journey/views.py
+++ OurJourney/journey/views.py
@@ -6,10 +6,14 @@ from datetime import datetime, timedelta
 from .google_calendar.cal_setup import get_calendar_service
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
+from django.contrib.auth.forms import AuthenticationForm, authenticate
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
+    # Bottom Images:
     image_objects = TakenAdventures.objects.all()
 
     paginator = Paginator(image_objects, 1)
@@ -28,10 +32,26 @@ def home(request):
     else: 
         prev_image = paginator.get_page(paginator.num_pages)
 
+    # Top Login
+    if request.method == "POST":
+        user_form = AuthenticationForm(request, data=request.POST)
+
+        if user_form.is_valid():
+            username = user_form.cleaned_data.get("username")
+            password = user_form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+            else:
+                redirect('/journey/')
+    user_form = AuthenticationForm()
+
     context = {
         'prev_image': prev_image,
         'active_image': active_image,
-        'next_image': next_image
+        'next_image': next_image,
+        'user_form': user_form
     }
     return render(request, 'journey/home.html', context)
 
@@ -56,10 +76,12 @@ class AddAdventure(CreateView):
 
 
 def calendar(request):
-
-    context = {
-    }
-    return render(request, 'journey/calendar.html', context)
+    if auth_check(request):
+        context = {
+        }
+        return render(request, 'journey/calendar.html', context)
+    else:
+        return redirect('/journey/')
 
 
 def detail(request, id):
@@ -93,7 +115,27 @@ def detail(request, id):
     return render(request, 'journey/detail.html', {'adv': adv})
 
 
+def adventures_taken(request):
+    adv = Adventures.objects.all()
+
+    # Search
+    adv_name = request.GET.get('adv_name')
+    if adv_name != '' and adv_name is not None:
+        adv = adv.filter(title__icontains=adv_name)
+
+    paginator = Paginator(adv, 9)
+    page = request.GET.get('page')
+    adv = paginator.get_page(page)
+
+    return render(request, 'journey/adventures_taken.html', {'adv': adv})
+
+
 def home_redirect(request):
     return redirect('/journey/')
+
+
+def auth_check(request):
+    if request.user.is_authenticated:
+        return True
 
 
