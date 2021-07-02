3c50d895c29f1c222e7d14f3bf42767b36f926e9
jacobskr <31374947+jacobskr@users.noreply.github.com>
2021-07-02 12:56:50 -0400

    Made admin view readable

--- OurJourney/journey/models.py
+++ OurJourney/journey/models.py
@@ -50,3 +50,5 @@ class TakenAdventures(models.Model):
     location = models.CharField(default="", max_length=200)
     date = models.CharField(default="", max_length=200)
 
+    def __str__(self):
+        return self.caption
